17cd87e06060dc69f3d9a1e42b798e901c57f95d
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-06-01 08:47:47 -0700

    Update yung_Coinex7.4h_sma5.20.25.py

--- yung_Coinex7.4h_sma5.20.25.py
+++ yung_Coinex7.4h_sma5.20.25.py
@@ -3,11 +3,13 @@ import platform
 import sys
 import time
 import pickle
-import pandas as pd
+import json
 
+import pandas as pd
+import numpy as np
 
 from client import RequestsClient
-from . import correo
+import correo
 '''
 
 import requests
@@ -15,7 +17,7 @@ import hashlib
 import hmac
 from urllib.parse import urlencode
 from hashlib import sha256
-import json
+
 
 '''
 
@@ -45,15 +47,20 @@ class SwingTradingBot:
         self.client=RequestsClient(access_id=access_id,secret_key=secret_key)
         self.apalancamiento=apalancamiento
         self.ganancia=0
+        self.current_price=None
         self.current_operation=None
         self.id_operation=None
         self.open_price=None
         self.analisis=1
+
+        self.cant_opr=0
+        self.cant_opr_win=0
+        self.cant_opr_over=0
+
         self.save_state()
 
 
-    def get_current_price(self):
-        pass
+    
 
     def get_balance(self):
         pass
@@ -74,7 +81,7 @@ class SwingTradingBot:
 
 
 
-
+    
 
     #LISTO
     def get_data(self,size,temporalidad):
@@ -89,95 +96,62 @@ class SwingTradingBot:
             "{url}{request_path}".format(url=self.client.url, request_path=request_path),
             params=params,
         )
-        return response
+        data=response.json().get("data")
+        ohlcv_df = pd.DataFrame(data)
+        # Convertir las columnas de precios y volumen a numérico
+        ohlcv_df['close'] = pd.to_numeric(ohlcv_df['close'])
+        ohlcv_df['high'] = pd.to_numeric(ohlcv_df['high'])
+        ohlcv_df['low'] = pd.to_numeric(ohlcv_df['low'])
+        ohlcv_df['open'] = pd.to_numeric(ohlcv_df['open'])
+        ohlcv_df['volume'] = pd.to_numeric(ohlcv_df['volume'])
+        self.current_price = ohlcv_df['close'].iloc[-1]
+        return ohlcv_df
 
 
-    def get_indicator(self):
-        '''
-        Período de velas.
-        Uno de ["1min", "3min", "5min", "15min", "30min", "1hour", "2hour", "4hour", "6hour", 
-        "12hour", "1day", "3day", "1week"]
-        '''
-        precios_cierre = self.get_data(30,"4hour")
-        while precios_cierre.__len__()==0:
-            print("[#] reobteniendo los datos historicos....")
-            precios_cierre = self.get_data(30,"4h")
-        precios_cierre = [float(precio) for precio in precios_cierre]
-        current_price=float(precios_cierre[0])
-        precios_cierre
-        if self.delete_fast:
-            precios_cierre.pop(0)
-        
-        precios_cierre=precios_cierre[::-1]
-        
-
-        df = pd.DataFrame(precios_cierre, columns=['close'])
-        df['ema25'] = df['close'].ewm(span=25).mean()
-        df['ema5'] = df['close'].ewm(span=5).mean()
-        df['ema20'] = df['close'].ewm(span=20).mean()
-        # Devolverlos últimos valores de las EMAs como variables separadas
-        ema5 = df['ema5'].iloc[-1]
-        ema20 = df['ema20'].iloc[-1]
-        ema25 = df['ema25'].iloc[-1]
-        
-        balance = self.get_balance()
-        while balance == None:
-            print("[#] reobteniendo el balance....")
-            balance = self.get_balance()
-         # Devolver los valores calculados
-        return ema5,ema20,ema25,balance,current_price
-    
-   
     #ESTRATEGIA LISTA
     def trade(self):
-        ema5,ema20,ema25,balance,current_price=self.get_indicator()
-        self.current_price=current_price
+        data=self.get_data(500,"4hour")
+        patron=self.identificar_patron(data)
+        balance=7
+        s=""
         nueva=False
-        s=f"[#] Analisis # {self.analisis}\n"
-        self.analisis+=1
-        s+=f"[#] OPERACION ACTUAL: {self.current_operation}\n"
-        s+=f"[#] GANANCIA ACTUAL: {self.ganancia}\n"
-        s+=f"[#] PRECIO BTC-USDT: {current_price}\n"
-
-        
         if self.current_operation == "LONG":
-            if (ema5<ema20):
-                s+=self.close_operations(current_price)
+            if patron in ["venta","lateralizacion"]:
+                s+=self.close_operations(self.current_price)
                 nueva=True
             else:
-                s+=self.mantener(current_price)
-                #============================================
+                s+=self.mantener(self.current_price)
         elif self.current_operation == "SHORT":
-            if (ema5>ema20):
-                s+=self.close_operations(current_price)
+            if (patron in ["compra","lateralizacion"]):
+                s+=self.close_operations(self.current_price)
                 nueva=True
             else:
-                s+=self.mantener(current_price)
+                s+=self.mantener(self.current_price)
                 #============================================
 
                 
         if self.current_operation == None:
-            if (ema5>ema20 and ema20>ema25) and current_price>ema25 and balance*0.9>=2:
+            if patron=="compra" and balance*0.9>=2:
                 self.set_apalancamiento("LONG")
                 s+=self.open_long()
                 nueva=True
-            elif (ema5<ema20 and ema20<ema25) and current_price<ema25 and balance*0.9>=2:
+            elif patron=="venta" and balance*0.9>=2:
                 self.set_apalancamiento("SHORT")
                 s+=self.open_short()
                 nueva=True
             else:
-                s+=self.mantener(current_price)
+                s+=self.mantener(self.current_price)
                 #============================================
-        emas={
-            "[#] EMA 5: ":ema5,
-            "[#] EMA 20: ":ema20,
-            "[#] EMA 25: ":ema25,
-        }   
-        emas=dict(sorted(emas.items(), key=lambda item: item[1], reverse=True))
-        for clave,valor in emas.items():
-            s+=clave
-            s+=f"{valor}\n"
         
+        s=f"[#] Analisis # {self.analisis}\n"
+        self.analisis+=1
+        s+=f"[#] OPERACION ACTUAL: {self.current_operation}\n"
+        s+=f"[#] GANANCIA ACTUAL: {self.ganancia}\n"
+        s+=f"[#] PRECIO BTC-USDT: {self.current_price}\n"
+        s+=f"[#] OPERACIONES: {self.cant_opr}\n"
+        s+=f"[#] GANADAS: {self.cant_opr_win}\n"
+        s+=f"[#] PERDIDAS: {self.cant_opr_over}\n"        
+        s+=f"SEÑAL: {patron}\n"
         s+=f"[#] BALANCE: {balance} USDT\n"
         s+="\n--------------------------------------\n"
         if nueva == True and self.ENVIO_MAIL==True:
@@ -185,18 +159,64 @@ class SwingTradingBot:
         return s
 
 
+    def identificar_patron(self,ohlcv_df):
+        # Encontrar máximos y mínimos locales
+        # Asegurarse de que la ventana rodante tenga exactamente 5 elementos antes de aplicar la función lambda
+        maximos = ohlcv_df['high'].rolling(window=5, center=False).apply(
+            lambda x: x[2] if (len(x) == 5 and x[2] == x.max()) else np.nan, raw=True
+        )
+        minimos = ohlcv_df['low'].rolling(window=5, center=False).apply(
+            lambda x: x[2] if (len(x) == 5 and x[2] == x.min()) else np.nan, raw=True
+        )
+        # Últimos precios
+        ultimo_maximo = maximos.last_valid_index()
+        ultimo_minimo = minimos.last_valid_index()
+
+        # Última fila del dataframe para la señal actual
+        ultimo = ohlcv_df.iloc[-1]
+
+        # Reconocimiento de patrones
+        if pd.notnull(ultimo_maximo) and pd.notnull(ultimo_minimo):
+            if ultimo_maximo > ultimo_minimo:
+                # El último máximo es más reciente que el último mínimo
+                if ultimo['close'] > ohlcv_df.loc[ultimo_maximo, 'high']:
+                    return 'compra'  # Rompimiento alcista
+                elif ultimo['close'] < ohlcv_df.loc[ultimo_minimo, 'low']:
+                    return 'venta'  # Rompimiento bajista
+                else:
+                    return 'lateralizacion'  # Sin rompimiento claro
+            elif ultimo_minimo > ultimo_maximo:
+                # El último mínimo es más reciente que el último máximo
+                if ultimo['close'] < ohlcv_df.loc[ultimo_minimo, 'low']:
+                    return 'venta'  # Rompimiento bajista
+                elif ultimo['close'] > ohlcv_df.loc[ultimo_maximo, 'high']:
+                    return 'compra'  # Rompimiento alcista
+                else:
+                    return 'lateralizacion'  # Sin rompimiento claro
+        else:
+            return 'lateralizacion'  # No se encontraron patrones claros
+
+
     def close_operations(self,current_price):
         if self.Operar:
             self.close()
+        self.cant_opr+=1
         s=""
         s+=f">>>> CERRANDO POSICION {self.current_operation}\n"
+        estado=0
         if self.current_operation == "LONG":
-            self.ganancia+=current_price - self.open_price
-            s+=f"[#] ESTADO: {current_price - self.open_price}\n"
+            estado=current_price - self.open_price
+            self.ganancia+=estado
+            s+=f"[#] ESTADO: {estado}\n"
         else:
-            self.ganancia+=self.open_price - current_price
-            s+=f"[#] ESTADO: {self.open_price - current_price}\n"
+            estado=self.open_price - current_price
+            self.ganancia+=estado
+            s+=f"[#] ESTADO: {estado}\n"
         s+=f"[#] GANANCIA: {self.ganancia}\n"
+        if estado>0:
+            self.cant_opr_win+=1
+        else:
+            self.cant_opr_over+=1
         self.open_price=None
         self.current_operation=None
         self.save_state()
@@ -310,15 +330,15 @@ def run_bot():
     # Iniciar el bot
     while True:
         error=False
-        try:
-            print("\nPROCESANDO ANALISIS...")
-            s=bot.trade()
-            clear_console()
-            print(s)
-        except Exception as e:
-            clear_console()
-            print(f"Error: {str(e)}\n")
-            error=True
+        #try:
+        print("\nPROCESANDO ANALISIS...")
+        s=bot.trade()
+        clear_console()
+        print(s)
+        #except Exception as e:
+        #    clear_console()
+        #    print(f"Error: {str(e)}\n")
+        #    error=True
         print("Esperando para el próximo análisis...")
         if error:
             tiempo_espera=1
