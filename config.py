ec203001e30f597b8ade08ecd36dbc7249a83d83
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-06-06 09:07:28 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ access_id = "2BB2CDB4E9034D5C9EBB04041EBE5089"  # Replace with your access id
 secret_key = "CA2792E400D023DAD732CA41C4ED0B98B0CC638FF77D9C65"  # Replace with your secret key
 ENVIO_MAIL=True
 Operar=False
+incluir_precio_actual=False
 email="liranzaelias@gmail.com"
 simbol="BTCUSDT"
 size=30
